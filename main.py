40bb84ed25b12be10aa53a7b7770f4258842a52e
mschap <mschap@users.noreply.github.com>
2024-04-29 19:30:02 +0200

    v2

--- main.py
+++ main.py
@@ -2,15 +2,18 @@ import random
 import numpy as np
 import time
 
+# Define a custom exception class
+class myErrorMsg(Exception):
+    pass
 
 # Get indices without numpy, including check for empty list or non-numerical values
 def indices(data):
     # check if list is empty
     if not data:
-        return "error: list is empty"
+        raise myErrorMsg("list is empty")
     # check if list contains non numerical values (not int or float)
     if not all(isinstance(x, (int, float)) for x in data):
-        return "error: not all elements are int or float"
+        raise myErrorMsg("not all elements are int or float")
     # create list of indices
     indices = list(range(len(data)))
     return indices
@@ -20,12 +23,12 @@ def indices(data):
 def indicesnp(data):
     #check if list is empty
     if not data:
-        return "error: list is empty"
+        raise myErrorMsg("list is empty")
     # convert list to numpy array
     nparr = np.array(data)
     # check if numpy array contains non numerical values (not int or float)
     if not np.isreal(nparr).all():
-        return "error: not all elements are int or float (real)"
+        raise myErrorMsg("not all elements are int or float (real)")
     # create list of indices with numpy
     indices = np.arange(len(data))
     return indices
@@ -40,78 +43,141 @@ list3 = []
 for i in range(0, 1000000):
     list3.append(random.randint(0, 1000))
 
-print("Indices without numpy, list1")
-start = time.time_ns()
-ind_list1 = indices(list1)
-stop = time.time_ns()
-runtimeList1 = stop - start
-print(ind_list1)
+print("\nIndices without numpy, list1")
+try:
+    start = time.time_ns()
+    ind_list1 = indices(list1)
+    stop = time.time_ns()
+    runtimeList1 = stop - start
+    print(ind_list1)
+except myErrorMsg as e:
+    print("MyError: " + str(e))
 
-print("Indices without numpy, list2")
-start = time.time_ns()
-ind_list2 = indices(list2)
-stop = time.time_ns()
-runtimeList2 = stop - start
-print(ind_list2)
+print("\nIndices without numpy, list2")
+try:
+    start = time.time_ns()
+    ind_list2 = indices(list2)
+    stop = time.time_ns()
+    runtimeList2 = stop - start
+    print(ind_list2)
+except myErrorMsg as e:
+    print("MyError: " + str(e))
 
-print("Indices without numpy, list2a")
-start = time.time_ns()
-ind_list2a = indices(list2a)
-stop = time.time_ns()
-runtimeList2a = stop - start
-print(ind_list2a)
 
-print("Indices without numpy, list3")
-start = time.time_ns()
-ind_list3 = indices(list3)
-stop = time.time_ns()
-runtimeList3 = stop - start
-print(ind_list3)
+print("\nIndices without numpy, list2a")
+try:
+    start = time.time_ns()
+    ind_list2a = indices(list2a)
+    stop = time.time_ns()
+    runtimeList2a = stop - start
+    print(ind_list2a)
+except myErrorMsg as e:
+    print("MyError: " + str(e))
 
-print("Indices WITH numpy, list1")
-start = time.time_ns()
-ind_listnp1 = indicesnp(list1)
-stop = time.time_ns()
-runtimeListnp1 = stop - start
-print(ind_listnp1)
+input("\nPress any button to continue...")
 
-print("Indices WITH numpy, list2")
-start = time.time_ns()
-ind_listnp2 = indicesnp(list2)
-stop = time.time_ns()
-runtimeListnp2 = stop - start
-print(ind_listnp2)
+print("\nIndices without numpy, list3")
+try:
+    start = time.time_ns()
+    ind_list3 = indices(list3)
+    stop = time.time_ns()
+    runtimeList3 = stop - start
+    print(ind_list3)
+except myErrorMsg as e:
+    print("MyError: " + str(e))
 
-print("Indices WITH numpy, list2a")
-start = time.time_ns()
-ind_listnp2a = indicesnp(list2a)
-stop = time.time_ns()
-runtimeListnp2a = stop - start
-print(ind_listnp2a)
+print("\nIndices WITH numpy, list1")
+try:
+    start = time.time_ns()
+    ind_listnp1 = indicesnp(list1)
+    stop = time.time_ns()
+    runtimeListnp1 = stop - start
+    print(ind_listnp1)
+except myErrorMsg as e:
+    print("MyError: " + str(e))
 
-print("Indices WITH numpy, list3")
-start = time.time_ns()
-ind_listnp3 = indicesnp(list3)
-stop = time.time_ns()
-runtimeListnp3 = stop - start
-print(ind_listnp3)
+print("\nIndices WITH numpy, list2")
+try:
+    start = time.time_ns()
+    ind_listnp2 = indicesnp(list2)
+    stop = time.time_ns()
+    runtimeListnp2 = stop - start
+    print(ind_listnp2)
+except myErrorMsg as e:
+    print("MyError: " + str(e))
 
+print("\nIndices WITH numpy, list2a")
+try:
+    start = time.time_ns()
+    ind_listnp2a = indicesnp(list2a)
+    stop = time.time_ns()
+    runtimeListnp2a = stop - start
+    print(ind_listnp2a)
+except myErrorMsg as e:
+    print("MyError: " + str(e))
+
+print("\nIndices WITH numpy, list3")
+try:
+    start = time.time_ns()
+    ind_listnp3 = indicesnp(list3)
+    stop = time.time_ns()
+    runtimeListnp3 = stop - start
+    print(ind_listnp3)
+except myErrorMsg as e:
+    print("MyError: " + str(e))
+
+
+# Runtime Commparison
 print("\n\nRuntime Comparison")
 print("1: Not using numpy, list1\n2: Using numpy, list1")
-print("1: " + str(runtimeList1) + " ns or " + str(runtimeList1 / 1000000000) + " sec")
-print("2: " + str(runtimeListnp1) + " ns or " + str(runtimeListnp1 / 1000000000) + " sec\n")
+try:
+    print("1: " + str(runtimeList1) + " ns or " + str(runtimeList1 / 1000000000) + " sec")
+except Exception as e:
+    print("Something went wrong: " + str(e))
 
-print("1: Not using numpy, list2\n2: Using numpy, list2")
-print("1: " + str(runtimeList2) + " ns or " + str(runtimeList2 / 1000000000) + " sec")
-print("2: " + str(runtimeListnp2) + " ns or " + str(runtimeListnp2 / 1000000000) + " sec\n")
-
-print("1: Not using numpy, list2a\n2: Using numpy, list2a")
-print("1: " + str(runtimeList2a) + " ns or " + str(runtimeList2a / 1000000000) + " sec")
-print("2: " + str(runtimeListnp2a) + " ns or " + str(runtimeListnp2a / 1000000000) + " sec\n")
-
-print("1: Not using numpy, list3\n2: Using numpy, list3")
-print("1: " + str(runtimeList3) + " ns or " + str(runtimeList3 / 1000000000) + " sec")
-print("2: " + str(runtimeListnp3) + " ns or " + str(runtimeListnp3 / 1000000000) + " sec\n")
+try:
+    print("2: " + str(runtimeListnp1) + " ns or " + str(runtimeListnp1 / 1000000000) + " sec\n")
+except Exception as e:
+    print("Something went wrong: " + str(e))
 
 
-%input("Press any key to continue")
+
+print("\n1: Not using numpy, list2\n2: Using numpy, list2")
+try:
+    print("1: " + str(runtimeList2) + " ns or " + str(runtimeList2 / 1000000000) + " sec")
+except Exception as e:
+    print("Something went wrong: " + str(e))
+
+try:
+    print("2: " + str(runtimeListnp2) + " ns or " + str(runtimeListnp2 / 1000000000) + " sec\n")
+except Exception as e:
+    print("Something went wrong: " + str(e))
+
+
+
+
+print("\n1: Not using numpy, list2a\n2: Using numpy, list2a")
+try:
+    print("1: " + str(runtimeList2a) + " ns or " + str(runtimeList2a / 1000000000) + " sec")
+except Exception as e:
+    print("1: Something went wrong: " + str(e))
+
+try:
+    print("2: " + str(runtimeListnp2a) + " ns or " + str(runtimeListnp2a / 1000000000) + " sec\n")
+except Exception as e:
+    print("2: Something went wrong: " + str(e))
+
+
+
+print("\n1: Not using numpy, list3\n2: Using numpy, list3")
+try:
+    print("1: " + str(runtimeList3) + " ns or " + str(runtimeList3 / 1000000000) + " sec")
+except Exception as e:
+    print("1: Something went wrong: " + str(e))
+
+try:
+    print("2: " + str(runtimeListnp3) + " ns or " + str(runtimeListnp3 / 1000000000) + " sec\n")
+except Exception as e:
+    print("2: Something went wrong: " + str(e))
+
+# input("Press any key to continue")
